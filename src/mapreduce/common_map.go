8969fa040c113b4ecb14d7758a0031b9ebcb60e9
1988tianyuan <467651794@qq.com>
2019-05-05 16:31:56 +0800

    mapreduce: part1

--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,7 +1,11 @@
 package mapreduce
 
 import (
+	"encoding/json"
+	"fmt"
 	"hash/fnv"
+	"io/ioutil"
+	"os"
 )
 
 func doMap(
@@ -11,6 +15,48 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(filename string, contents string) []KeyValue,
 ) {
+	inBytes,err := ioutil.ReadFile(inFile)
+	var contents string
+	var resultKV []KeyValue
+	if err != nil {
+		fmt.Println("error happens when reading file of mapTask: ", err)
+	} else {
+		contents = string(inBytes)
+		// execute user-defined mapTask function
+		resultKV = mapF(inFile, contents)
+		encMap := make(map[string]*json.Encoder, nReduce)
+		rNames := make([]string, nReduce)
+		outFiles := make([]*os.File, nReduce)
+		for i:=0; i<nReduce; i++ {
+			rName := reduceName(jobName, mapTask, i)
+			rNames[i] = rName
+			file, err := os.Create(rName)
+			if err != nil {
+				fmt.Println("error happens when create result file of mapTask: ", err)
+				return
+			}
+			enc := json.NewEncoder(file)
+			encMap[rName] = enc
+			outFiles[i] = file
+		}
+		for _,item := range resultKV {
+			hash := ihash(item.Key)
+			enc := encMap[rNames[nReduce % hash]]
+			err := enc.Encode(&item)
+			if err != nil {
+				fmt.Printf("error happens when writing result file of mapTask:{%d}, error is {%v} \n", mapTask, err)
+				return
+			}
+		}
+		defer func() {
+			for _,file := range outFiles {
+				err := file.Close()
+				fmt.Println("io error happens when close result file of mapTask", err)
+			}
+		}()
+	}
+
+
 	//
 	// doMap manages one map task: it should read one of the input files
 	// (inFile), call the user-defined map function (mapF) for that file's
--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTask int, // which reduce task this is
@@ -7,7 +13,56 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	//
+	inFiles := make([]*os.File, nMap)
+	for i:=0; i<nMap; i++ {
+		reduceName := reduceName(jobName, i, reduceTask)
+		file,err := os.Create(reduceName)
+		if err != nil {
+			fmt.Printf("error happens when create input file of reductTask:{%d}, error is {%v} \n", reduceTask, err)
+			return
+		}
+		inFiles[i] = file
+	}
+	kvMap := make(map[string][]string)
+	for _,file := range inFiles {
+		dec := json.NewDecoder(file)
+		for dec.More() {
+			kv := new(KeyValue)
+			err := dec.Decode(kv)
+			if err != nil {
+				fmt.Printf("error happens when decode from intermediate input file:{%v}, error is {%v} \n", file, err)
+				break
+			}
+			values := kvMap[kv.Key]
+			if values == nil {
+				values = make([]string, 10)
+			}
+			values = append(values, kv.Value)
+			kvMap[kv.Key] = values
+		}
+	}
+
+	oFile,err := os.Create(outFile)
+	if err != nil {
+		fmt.Printf("error happens when create output file of reductTask:{%d}, error is {%v} \n", reduceTask, err)
+		return
+	}
+	enc := json.NewEncoder(oFile)
+	for key,values := range kvMap {
+		result := reduceF(key, values)
+		err := enc.Encode(KeyValue{key, result})
+		if err != nil {
+			fmt.Printf("error happens when create encode result of reductTask:{%d} to outFile:{%s}, error is {%v} \n", reduceTask, outFile, err)
+		}
+	}
+
+	defer func() {
+		for _,file := range inFiles {
+			_ = file.Close()
+		}
+		_ = oFile.Close()
+	}()
+
 	// doReduce manages one reduce task: it should read the intermediate
 	// files for the task, sort the intermediate key/value pairs by key,
 	// call the user-defined reduce function (reduceF) for each key, and
